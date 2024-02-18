7505a28c29d4506753517fe5f98849f07b31a55b
14kkrason <14krzysztof.krason@gmail.com>
2024-02-18 14:11:24 +0100

    feat(infrastructure):in-memory config adapter

--- src/app/services/example.service.ts
+++ src/app/services/example.service.ts
@@ -1,14 +1,12 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { ConfigImplementation } from "@infrastructure/config";
-import { Config } from "@app/ports";
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigImplementation } from '@infrastructure/config';
+import { Config } from '@app/ports';
 
 @Injectable()
 export class ExampleService {
-    constructor(
-        @Inject(ConfigImplementation) private readonly config: Config
-    ) {}
-    
-    public handle() {
-        return this.config.get('example');  
-    }
+  constructor(@Inject(ConfigImplementation) private readonly config: Config) {}
+
+  public handle() {
+    return this.config.get('example');
+  }
 }
